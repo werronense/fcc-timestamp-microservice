3853bcd0fd7ccbc60c058012bf70f19f12d2bb0c
Glitch (scratched-kitten) <none>
2019-06-17 18:07:22 +0000

    💍🐑 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// timpestamp API code
+app.get("/api/timestamp/:date_string?", function(req, res) {
+  function dateJson(inputDate) {
+    return {"unix": inputDate.getTime(), "utc": inputDate.toUTCString()}
+  }
+  
+  if (!req.params.date_string) {
+    res.json(dateJson(new Date()));
+  } else {
+    res.json(dateJson(new Date(req.params.date_string)));
+  }
+  
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
